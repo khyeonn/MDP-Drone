b59f176c7053948a0c8f22e5608e2af5a67a7e69
Max Kim <maxkim7100@gmail.com>
2024-05-01 23:12:06 -0400

    Added environment termination handling

--- drone.jl
+++ drone.jl
@@ -20,10 +20,11 @@ end
 
 struct DroneEnv
     size::Tuple{Int, Int}
-    goal::Tuple{Float64, Float64, Float64}
+    target::Tuple{Float64, Float64, Float64}
     max_velocity::Float64
     max_rotation_speed::Float64
     discount::Float64
+    isterminal::Bool
     # obstacles::Vector{Tuple{Float64, Float64, Float64}}
 end
 
@@ -31,12 +32,13 @@ function DroneEnv(;size = DEFAULT_SIZE,
         max_velocity = 1.0,
         max_rotation_speed = 0.1,
         discount = 0.95,
-        goal_radius = 5.0
+        target_radius = 5.0
         )
     step_size = 0.001
-    goal_x, goal_y = rand(1:step_size:size[1]), rand(1:step_size:size[2])
-    goal = (goal_x, goal_y, goal_radius)
-    return DroneEnv(size, goal, max_velocity, max_rotation_speed, discount)
+    target_x, target_y = rand(1:step_size:size[1]), rand(1:step_size:size[2])
+    target = (target_x, target_y, target_radius)
+    isterminal = false
+    return DroneEnv(size, target, max_velocity, max_rotation_speed, discount, isterminal)
 end
 
 
@@ -52,17 +54,30 @@ function POMDPs.transition(env::DroneEnv, state::DroneState, action::DroneAction
     x_new = state.x + v_new*cos(theta_new)
     y_new = state.y + v_new*sin(theta_new)
 
-    return DroneState(x_new, y_new, v_new, theta_new)
+    new_state = DroneState(x_new, y_new, v_new, theta_new)
+
+    if isterminal(env, new_state)
+        env.isterminal = true
+    end
+
+    return new_state
 end
 
-function POMDPs.reward(state::DroneState, target)
-    distance_to_target = sqrt((state.x - target[1])^2 + (state.y - target[2])^2)
+function isterminal(env::DroneEnv, state::DroneState)
+    distance_to_target = sqrt((state.x - env.target[1])^2 + (state.y - env.target[2])^2)
 
-    if distance_to_target < target[3]
+    return distance_to_target <= env.target[3]
+end
+
+function POMDPs.reward(env::DroneEnv, state::DroneState)
+    if isterminal(env, state)
         return 100.0
     else
         return -0.1
     end
 end
+
+
+POMDPs.discount(env::DroneEnv) = env.discount
 
 end # module
--- test.jl
+++ test.jl
@@ -2,14 +2,15 @@ if !@isdefined Drone
     include("Drone.jl")
 end
 using POMDPs
-using .Drone
+using .Drone: isterminal, discount
 
-m = DroneEnv()
+env = DroneEnv()
 
 target = (10.0, 10.0, 5.0) # target region (x, y, radius)
 initial_state = DroneState(0.0, 0.0, 0.0, 0.0) # state is (x, y, v, theta)
 
 action = DroneAction(0.5, 0.5) # actions are (accelerate, turn). turn action is in radians
 
-new_state = POMDPs.transition(m, initial_state, action)
-reward = POMDPs.reward(new_state, target) 
+new_state = POMDPs.transition(env, initial_state, action)
+reward = POMDPs.reward(env, new_state) 
+
